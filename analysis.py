e7475635ffce6d7d8cf4b16b75b48e7950b828a1
unknown <gerball2193@gmail.com>
2023-05-08 22:29:49 +0100

    PANDS-PROJECT

--- analysis.py
+++ analysis.py
@@ -11,25 +11,21 @@ Color the scatter plots by class
 Create a function to generate scatter plots for any two variables and save them to png files
 Refactor the code to make it more modular and reusable'''
 
-
-
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Load the iris dataset
-iris = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+iris = pd.read_csv('iris.data', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+# Define the function to predict the species based on sepal length
+def predict_species(sepal_length):
+    if sepal_length < 5.84:
+        return 'Iris-setosa'
+    elif sepal_length < 6.62:
+        return 'Iris-versicolor'
+    else:
+        return 'Iris-virginica'
 
-# Group the data by class and get the mean of each feature
-iris_mean = iris.groupby('class').mean()
+# Apply the function to the sepal length column and store the predicted species in a new column
+iris['predicted_species'] = iris['sepal_length'].apply(predict_species)
 
-# Create a bar chart of the mean feature values for each class
-iris_mean.plot(kind='bar')
-plt.title('Mean Feature Values for Each Iris Class')
-plt.xlabel('Class')
-plt.ylabel('Feature Value')
-
-# Save the bar chart as a PNG file
-plt.savefig('iris_bar_chart.png')
-
-# Display the bar chart
-plt.show()
+# Save the dataframe to a CSV file
+iris.to_csv('iris_predicted_species.csv', index=False)
